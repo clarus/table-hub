2f56764498c7cb5adb8c6dbd3bb5c3cf7eaa52f0
Guillaume Claret <dev@clarus.me>
2013-12-17 23:15:21 +0100

    Resource Users in /api

--- web-app/config/routes.rb
+++ web-app/config/routes.rb
@@ -1,5 +1,7 @@
 WebApp::Application.routes.draw do
-  resources :users
+  scope :api do
+    resources :users
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
